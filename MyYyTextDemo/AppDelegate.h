f51abeda853874ae15a97963f6d7e44feafa6cba
yanchao <yanchao@dajiazhongyi.com>
2018-07-04 18:25:30 +0800

    First commit.

---
+++ MyYyTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyYyTextDemo
+//
+//  Created by yan on 2018/7/4.
+//  Copyright © 2018 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyYyTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyYyTextDemo
+//
+//  Created by yan on 2018/7/4.
+//  Copyright © 2018 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
